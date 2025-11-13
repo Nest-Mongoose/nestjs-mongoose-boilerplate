fdd82776bdb248ac53828827e78683a2d0a816ea
snowmanxm <xm@angel-club.org>
2025-11-13 19:23:44 +0000

    chore: remove unnecessary import

--- src/web/features/auth/auth.module.ts
+++ src/web/features/auth/auth.module.ts
@@ -3,7 +3,6 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
-import { DatabaseModule } from '@/core/database/database.module';
 import { ENV } from '@/shared/enums';
 
 import { AuthController } from './auth.controller';
@@ -13,7 +12,6 @@ import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
-    DatabaseModule,
     PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
