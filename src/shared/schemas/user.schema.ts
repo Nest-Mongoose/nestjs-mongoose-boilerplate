a911904ceaf3530c8e63429e9a747ad161bb41cd
snowmanxm <xm@angel-club.org>
2025-11-13 19:32:57 +0000

    feat: share same class for dto and schema

--- src/shared/schemas/user.schema.ts
+++ src/shared/schemas/user.schema.ts
@@ -1,5 +1,9 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsEnum, IsMongoId, IsOptional, IsString, MaxLength } from 'class-validator';
 import { HydratedDocument } from 'mongoose';
+
+import { GENDER } from '@/shared/enums';
 
 export type UserDocument = HydratedDocument<User>;
 
@@ -10,19 +14,34 @@ export type UserDocument = HydratedDocument<User>;
   toObject: { virtuals: true },
 })
 export class User {
+  @IsMongoId()
+  @IsOptional()
+  @ApiProperty({ description: 'ID', default: '66e1c5a0809bae0741157574' })
   id?: string;
 
   @Prop({ type: String, required: true, unique: true })
+  @IsEmail()
+  @MaxLength(100)
+  @ApiProperty({ description: 'Email', default: 'john@nest.com' })
   email: string;
 
   @Prop({ type: String, required: true })
+  @IsString()
+  @MaxLength(100)
+  @ApiProperty({ description: 'Password' })
   password: string;
 
   @Prop({ type: String, required: true })
+  @IsString()
+  @MaxLength(100)
+  @ApiProperty({ description: 'Name', default: 'John' })
   name: string;
 
   @Prop({ type: String, required: false })
-  gender?: string;
+  @IsEnum(GENDER)
+  @IsOptional()
+  @ApiProperty({ description: 'Gender', enum: GENDER })
+  gender?: GENDER;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
